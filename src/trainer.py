5bba5ed0b7d8cdaafa7f16a0c8fa446fd792cd00
Yannick Wattenberg <wattenberg@ywattenberg.com>
2023-11-16 10:11:53 +0100

    add support for tensorboard and use tqdm

--- src/trainer.py
+++ src/trainer.py
@@ -4,6 +4,8 @@ from datetime import datetime
 from torch.utils.data import DataLoader, random_split, Subset
 import pandas as pd
 import numpy as np
+from tqdm import tqdm
+from torch.utils.tensorboard import SummaryWriter
 
 
 class Trainer:
@@ -14,32 +16,34 @@ class Trainer:
         test_data,
         loss_fn,
         optimizer,
-        split_test: float = None,
-        device=None,
-        batch_size=32,
-        epochs=10,
-        shuffle=True,
-        name=None,
-        test_metrics=None,
-        test_metric_names=None,
-        scheduler=None,
-        epochs_between_safe=1,
-        batches_between_safe=None,
-        split_random=False,
+        config,
         generator=torch.Generator().manual_seed(1337),
+        
     ):
+        # split_test: float = None,
+        # device=None,
+        # batch_size=32,
+        # epochs=10,
+        # shuffle=True,
+        # name=None,
+        # test_metrics=None,
+        # test_metric_names=None,
+        # scheduler=None,
+        # epochs_between_safe=1,
+        # batches_between_safe=None,
+        # split_random=False,
         if model == None or train_data == None:
             raise Exception("Model and train_data must be specified")
 
         if test_data == None:
-            if split_test == None:
+            if "split_test" not in config:
                 raise Exception("Must specify either test_data or split_train")
-            elif split_test != None:
+            else:
                 # warnings.warn("Test data is not specified, will split train data into train and validation")
-                train_len = int(len(train_data) * (1 - split_test))
+                train_len = int(len(train_data) * (1 - config["split_test"]))
                 test_len = len(train_data) - train_len
 
-            if split_random:
+            if config["split_random"]:
                 train_data, test_data = random_split(train_data, (train_len, test_len))
 
             else:
@@ -54,86 +58,90 @@ class Trainer:
         else:
             self.optimizer = optimizer
 
-        if device == None:
+        if "device" not in config:
             self.device = "cuda" if torch.cuda.is_available() else "cpu"
         else:
-            self.device = device
+            self.device = config["device"]
 
-        if name == None:
+        if "name" not in config:
             self.name = datetime.now().strftime("%Y-%m-%d_%H")
         else:
-            self.name = name
+            self.name = config["name"]
 
-        if test_metrics == None:
+        if "test_metrics" not in config:
             self.test_metrics = [loss_fn]
         else:
-            self.test_metrics = test_metrics
+            self.test_metrics = config["test_metrics"]
 
-        if test_metric_names == None:
+        if "test_metric_names" not in config:
             self.test_metric_names = [
                 f"Metric {i}" for i in range(len(self.test_metrics))
             ]
         else:
-            assert len(test_metric_names) == len(self.test_metrics)
-            self.test_metric_names = test_metric_names
+            assert len(config["test_metric_names"]) == len(self.test_metrics)
+            self.test_metric_names = config["test_metric_names"]
 
-        if scheduler == None:
-            self.scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-                self.optimizer, "min", patience=2
-            )
+        if "scheduler" not in config:
+            self.scheduler = None
         else:
-            self.scheduler = scheduler
+            self.scheduler = config["scheduler"]
 
-        self.batch_size = batch_size
-        self.num_epochs = epochs
+        if "tensorboard_path" in config:
+            self.writer = SummaryWriter(log_dir=config["tensorboard_path"])
+        else:
+            self.writer = None
+
+        self.batch_size = config["batch_size"]
+        self.num_epochs = config["epochs"]
         self.model = model.to(self.device)
         self.loss_fn = loss_fn
 
         self.train_dataloader = DataLoader(
-            train_data, batch_size=self.batch_size, shuffle=shuffle, num_workers=16, persistent_workers=True
+            train_data, batch_size=self.batch_size, shuffle=config["shuffle"], num_workers=16, persistent_workers=True
         )
         self.test_dataloader = DataLoader(
-            test_data, batch_size=self.batch_size, shuffle=shuffle, num_workers=16
+            test_data, batch_size=self.batch_size, shuffle=config["shuffle"], num_workers=16
         )
-        self.epochs_between_safe = epochs_between_safe
-        self.batches_between_safe = batches_between_safe
+        self.epochs_between_safe = config["epochs_between_safe"]
+        self.batches_between_safe = config["batches_between_safe"]
 
         self.test_scores = pd.DataFrame(columns=(["epoch"] + self.test_metric_names))
         self.train_loss = pd.DataFrame(
             columns=["Epoch", "Batch", "Curr Loss", "Running Loss"]
         )
 
-    def train_loop(self):
+    def train_loop(self, epoch=0):
         size = len(self.train_dataloader.dataset)
         time_at_start = time.time() * 1000
+        
         self.model.train()
         running_loss = np.array([])
-        for batch, (input, y) in enumerate(self.train_dataloader):
-            self.optimizer.zero_grad()
-            pred = self.model(input.to(self.device))
-            y = y.to(self.device)
-            loss = self.loss_fn(pred, y)
-            loss.backward()
-            self.optimizer.step()
-            running_loss = np.append(running_loss, loss.item())
-            if (batch * self.batch_size) % (size * 0.1) < self.batch_size:
-                current = batch * len(input[0])
-                print(
-                    f"loss: {(running_loss.sum()/(batch + 1)):>7f}  [{current:>5d}/{size:>5d}]"
-                )
-                run_time = time.time() * 1000 - time_at_start
-                print(
-                    f"time running: {run_time}, time per elem: {run_time/(current+1)}"
-                )
 
-            if (
-                self.batches_between_safe is not None
-                and batch % self.batches_between_safe == 0
-                and batch > self.batches_between_safe - 1
-            ):
-                print("saving model...")
-                torch.save(self.model.state_dict(), f"models/tmp_model_weights.pth")
-        return running_loss
+        with tqdm(self.train_dataloader, desc=f"Epoch {epoch}") as pbar:
+            pbar_dict = {}
+            for batch, (input, y) in enumerate(pbar):
+                self.optimizer.zero_grad()
+                pred = self.model(input.to(self.device))
+                y = y.to(self.device)
+                loss = self.loss_fn(pred, y)
+                loss.backward()
+                self.optimizer.step()
+                running_loss = np.append(running_loss, loss.item())
+                if (batch * self.batch_size) % (size * 0.1) < self.batch_size:
+                    current = batch * len(input[0])
+                    pbar_dict["Running Loss"] = running_loss.sum()/(batch + 1)
+                    run_time = time.time() * 1000 - time_at_start
+                    pbar.set_postfix(pbar_dict)
+                    if self.writer is not None:
+                        self.writer.add_scalar("Running Loss/train", running_loss.sum()/(batch + 1), epoch * len(self.train_dataloader) + batch)
+                if (
+                    self.batches_between_safe is not None
+                    and batch % self.batches_between_safe == 0
+                    and batch > self.batches_between_safe - 1
+                ):
+                    print("saving model...")
+                    torch.save(self.model.state_dict(), f"models/tmp_model_weights.pth")
+            return running_loss
 
     def test_loop(self):
         size = len(self.test_dataloader.dataset)
@@ -141,19 +149,21 @@ class Trainer:
         test_loss = [0 for _ in self.test_metrics]
         self.model.eval()
         with torch.no_grad():
-            for batch, (input, y) in enumerate(self.test_dataloader):
-                pred = self.model(input.to(self.device))
-                y = y.to(self.device)
-                for i, metric in enumerate(self.test_metrics):
-                    loss = metric(pred, y)
-                    test_loss[i] += loss.item()
-                if (batch * self.batch_size) % (size * 0.25) < self.batch_size:
+            with tqdm(self.test_dataloader, desc="Test") as pbar:
+                pbar_dict = {}
+                for batch, (input, y) in enumerate(pbar):
+                    pred = self.model(input.to(self.device))
+                    y = y.to(self.device)
                     for i, metric in enumerate(self.test_metrics):
-                        loss = test_loss[i] / (batch + 1)
-                        print(
-                            f"{self.test_metric_names[i]}: {loss:>7f}  [{batch:>5d}/{num_batches:>5d}]"
-                        )
-
+                        loss = metric(pred, y)
+                        test_loss[i] += loss.item()
+                    if (batch * self.batch_size) % (size * 0.2) < self.batch_size:
+                        for i, metric in enumerate(self.test_metrics):
+                            loss = test_loss[i] / (batch + 1)
+                            pbar_dict[self.test_metric_names[i]] = loss
+                            if self.writer is not None:
+                                self.writer.add_scalar(f"Running Loss/test_{self.test_metric_names[i]}", loss, batch)
+                        pbar.set_postfix(pbar_dict)
         if self.scheduler != None:
             self.scheduler.step(test_loss[0])
         avg_test_loss = {}
@@ -161,6 +171,8 @@ class Trainer:
             loss = test_loss[i] / num_batches
             avg_test_loss[self.test_metric_names[i]] = loss
             print(f"{self.test_metric_names[i]}: {loss:>7f}")
+            if self.writer is not None:
+                self.writer.add_scalar(f"Loss/test_{self.test_metric_names[i]}", loss, batch)
         return avg_test_loss
 
     def train_test(self):
@@ -191,7 +203,6 @@ class Trainer:
                         )
 
                 except Exception as e:
-                    print(f"Baka")
                     print(e)
 
                 self.test_scores = pd.concat(
