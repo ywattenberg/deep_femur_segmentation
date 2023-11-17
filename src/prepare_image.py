696509decca39242eb295c000dab3ed97a15c8eb
Yannick Wattenberg <wattenberg@ywattenberg.com>
2023-11-17 10:22:05 +0100

    update preprocessing for dataset

--- src/prepare_image.py
+++ src/prepare_image.py
@@ -1,18 +1,32 @@
 import argparse
 import SimpleITK as sitk
 import yaml
+import numpy as np
 
 from preprocessing.preprocessing import safe_image_to_np_series
 from preprocessing.calcualte_statistics import calculate_statistics
+from preprocessing.utils import image_to_array
 
 if __name__ == "__main__":
     # Parse path to image and output path
     parser = argparse.ArgumentParser()
     parser.add_argument("input", help="Path to the image to convert.")
     parser.add_argument("output", help="Path to the output files.")
+
     args = parser.parse_args()
-    image = sitk.ReadImage(args.input)
+    image = image_to_array(sitk.ReadImage(args.input))
+    original_min = np.min(image)
+    original_max = np.max(image)
+
+    # shift image to [0, 1] range
+    image = (image - min) / (max - min)
+
     stats = calculate_statistics(image)
+
+    #safe original stats for denomalization if neccessary
+    stats["original_min"] = original_min
+    stats["original_max"] = original_max
+
     print(stats)
     # Safe the statistics
     with open(args.output + "/statistics.yml", "w") as file:
--- src/preprocessing/calcualte_statistics.py
+++ src/preprocessing/calcualte_statistics.py
@@ -8,17 +8,18 @@ def _get_intensity_statistics(image: np.ndarray):
     return {
         "median": np.median(image),
         "mean": np.mean(image),
+        "std": np.std(image),
         "min": np.min(image),
         "max": np.max(image),
         "percentile_99_5": np.percentile(image, 99.5),
         "percentile_00_5": np.percentile(image, 0.5)
     }
 
-def calculate_statistics(image: sitk.Image | np.ndarray, sample: str ):
+def calculate_statistics(image: sitk.Image | np.ndarray):
     if isinstance(image, sitk.Image):
         image = image_to_array(image)
     
     statistics = {
-        sample: _get_intensity_statistics(image)
+        "intensity": _get_intensity_statistics(image)
     }
     return statistics
