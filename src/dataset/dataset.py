9562a3903fe12b58016cab8a61f6fcbe4d4e7048
Yannick Wattenberg <wattenberg@mailbox.org>
2023-12-06 12:55:52 +0100

    change dataset to not use zip files

--- src/dataset/dataset.py
+++ src/dataset/dataset.py
@@ -24,11 +24,15 @@ class FemurImageDataset(Dataset):
         self._config = config
         self._split = split
         self._use_accelerator = config["use_accelerator"]
-        self._input_size = [get_inital_crop_size(config["input_size"]) for i in range(3)]
-        self._output_size = [get_inital_crop_size(config["output_size"]) for i in range(3)]
+        if self._split == "train" or self._split == "val":
+            self._input_size = [get_inital_crop_size(config["input_size"]) for _ in range(3)]
+            self._output_size = [get_inital_crop_size(config["output_size"]) for _ in range(3)]
+        else:
+            self._input_size = config["input_size"]
+            self._output_size = config["output_size"]
 
         # 
-        self._scale_factor = [a/b for a,b in zip(self._output_size, self._input_size)]
+        self._scale_factor = [np.ceil(a/b) for a,b in zip(self._output_size, self._input_size)]
 
         self._base_path = config["base_path"]
         
@@ -63,26 +67,20 @@ class FemurImageDataset(Dataset):
             name = os.path.basename(HRpQCT_folder)
             
             # get zip files
-            HRpQCT_zip = os.path.join(HRpQCT_folder, f"{name}.zip")
-            PCCT_zip = os.path.join(PCCT_folder, f"{name}.zip")
+            HRpQCT_f = os.path.join(HRpQCT_folder, f"{name}")
+            PCCT_f = os.path.join(PCCT_folder, f"{name}")
 
-            # Check if zip files exist
-            if not os.path.exists(HRpQCT_zip):
-                raise ValueError(f"Zip file {HRpQCT_zip} does not exist.")
-            if not os.path.exists(PCCT_zip):
-                raise ValueError(f"Zip file {PCCT_zip} does not exist.")
+            files_in_folder = len(os.listdir(HRpQCT_f))
+            pcct_files = len(os.listdir(PCCT_f))
             
             # Get number of files in zip files
-            with zipfile.ZipFile(HRpQCT_zip, "r") as f:
-                files_in_folder = len(f.infolist())
-            with zipfile.ZipFile(PCCT_zip, "r") as f:
-                pcct_files = len(f.infolist())
-            print(f"Found {files_in_folder} files in {HRpQCT_folder} and {pcct_files} files in {PCCT_folder}")
-            self.PCCT_paths[i] = os.path.join(PCCT_folder, f"{name}.zip")
-            self.HRpQCT_paths[i] = os.path.join(HRpQCT_folder, f"{name}.zip")
 
-            self.slice_from_range[self.PCCT_paths[i]] = (self.length, self.length + pcct_files, i)
-            self.length += files_in_folder
+    
+            self.PCCT_paths[i] = os.path.join(PCCT_folder, f"{name}")
+            self.HRpQCT_paths[i] = os.path.join(HRpQCT_folder, f"{name}")
+
+            self.slice_from_range[self.PCCT_paths[i]] = (self.length, self.length + pcct_files - self._input_size[0], i)
+            self.length +=  pcct_files - self._input_size[0]
 
 
             
@@ -98,33 +96,41 @@ class FemurImageDataset(Dataset):
         
         assert start >= 0, "Start index must be greater than 0."
         # Calculate padding needed to get to the expected size
-        to_extend_back = 0
-        if end > num_slices_in_folder:
-            to_extend_back = end - num_slices_in_folder
-            end = num_slices_in_folder
-
-        
+        # if end > num_slices_in_folder:
+        #     end = num_slices_in_folder
 
         # TODO: Multithreading? Time this.
-        with zipfile.ZipFile(PCCT_folder, "r") as f:
-            for i in range(start, end):
-                with f.open(f"{i}.npy") as file:
-                    PCCT_images.append(np.load(file))
+        # with zipfile.ZipFile(PCCT_folder, "r") as f:
+        #     for i in range(start, end):
+        #         with f.open(f"{i}.npy") as file:
+        #             PCCT_images.append(np.load(file))
 
 
         # Load double the range from the HRpQCT folder
-        with zipfile.ZipFile(HRpQCT_folder, "r") as f:
-            for i in range(int(start*self._scale_factor[0]), int(end*self._scale_factor[0])+1):
-                with f.open(f"{i}.npy") as file:
-                    HRpQCT_images.append(np.load(file))
-        # PCCT_images = [np.zeros_like(PCCT_images[0]) for _ in range(to_extend_start)] + PCCT_images
-        PCCT_images = PCCT_images + [np.zeros_like(PCCT_images[0]) for _ in range(to_extend_back)]
-        HRpQCT_images =  HRpQCT_images + [np.zeros_like(HRpQCT_images[0]) for _ in range(to_extend_back)]
+        # with zipfile.ZipFile(HRpQCT_folder, "r") as f:
+        #     for i in range(int(start*self._scale_factor[0]), int(end*self._scale_factor[0])):
+        #         with f.open(f"{i}.npy") as file:
+
+        #             HRpQCT_images.append(np.load(file))
+
+        for i in range(start, end):
+            PCCT_images.append(np.load(os.path.join(PCCT_folder, f"{i}.npy")))
+        for i in range(int(start*self._scale_factor[0]), int(end*self._scale_factor[0])):
+            HRpQCT_images.append(np.load(os.path.join(HRpQCT_folder, f"{i}.npy")))
+        # print(f"Slices {start} to {end} loaded from {PCCT_folder} and {int(start*self._scale_factor[0])} to {int(end*self._scale_factor[0])} from  {HRpQCT_folder}")
+        
+        missing_pcct = self._input_size[0] - len(PCCT_images)
+        missing_hrpqct = self._output_size[0] - len(HRpQCT_images)
+        assert missing_pcct <= 0, "Missing PCCT images must be greater than 0."
+        assert missing_hrpqct <= 0, "Missing HRpQCT images must be greater than 0."
+        PCCT_images = PCCT_images + [np.zeros_like(PCCT_images[0]) for _ in range(missing_pcct)]
+        HRpQCT_images =  HRpQCT_images + [np.zeros_like(HRpQCT_images[0]) for _ in range(missing_hrpqct)]
 
         return PCCT_images, HRpQCT_images
     
     
     def __getitem__(self, index):
+        # time_at_start = time.time()
         folder = None
         sample = None
         for name, (start, end, folder_index) in self.slice_from_range.items():
@@ -132,7 +138,7 @@ class FemurImageDataset(Dataset):
                 folder = name
                 sample = folder_index
                 break
-        
+
         if folder is None:
             raise ValueError(f"Index {index} is out of range.")
         
@@ -142,26 +148,32 @@ class FemurImageDataset(Dataset):
         PCCT_path = folder
         HRpQCT_path = self.HRpQCT_paths[sample]
         # Load the images
+        # time_at_load_folder = time.time()
+        # print(f"Time to get to load folder: {time_at_load_folder - time_at_start}ms")
         PCCT_images, HRpQCT_images = self._load_range_from_folder(PCCT_path, HRpQCT_path, index, index + self._input_size[0])
         PCCT_images = np.stack(PCCT_images, axis=0)
         HRpQCT_images = np.stack(HRpQCT_images, axis=0)
+        # print(f"Time to load folder: {time.time() - time_at_load_folder}ms")
         # Apply augmentation
 
         PCCT_images = np.expand_dims(PCCT_images,0)
         HRpQCT_images = np.expand_dims(HRpQCT_images, 0)
+
+        # time_at_aug = time.time()
         if self.augmentation is not None:
             aug = self.augmentation({"image": PCCT_images, "labels": HRpQCT_images})
             PCCT_images = aug["image"]
             HRpQCT_images = aug["labels"]
-
+        # print(f"Time to augment: {time.time() - time_at_aug}ms")
         # If images are not tensors convert them to tensors
         if not torch.is_tensor(PCCT_images):
             PCCT_images = torch.from_numpy(PCCT_images)
         if not torch.is_tensor(HRpQCT_images):
             HRpQCT_images = torch.from_numpy(HRpQCT_images)
 
-        PCCT_images = PCCT_images.to(self._device, dtype=torch.float32)
-        HRpQCT_images = HRpQCT_images.to(self._device, dtype=torch.float32)
+        PCCT_images = PCCT_images.to(dtype=torch.float32)
+        HRpQCT_images = HRpQCT_images.to(dtype=torch.float32)
+        # print(f"Time to get item: {time.time() - time_at_start}ms")
         return PCCT_images, HRpQCT_images
 
 
