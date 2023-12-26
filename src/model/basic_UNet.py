7dcf6fcb8ec9c5a62142816e9597bc4185c0755d
Yannick Wattenberg <wattenberg@ywattenberg.com>
2023-12-26 11:58:58 +0100

    add new model that has a skip connection at top layer

--- src/model/basic_UNet.py
+++ src/model/basic_UNet.py
@@ -104,7 +104,69 @@ class UpsampleUNet(nn.Module):
 
     def forward(self, x):
         return self.UNet(x)
+    
 
+
+class UpsampleUNet_new(unet.UNet):
+    def __init__(self, in_channels, out_channels, config) -> None:
+        self._config = config
+        if "model" in config:
+            self._config = config["model"]
+        super
+        super().__init__(
+            spatial_dims=self._config["spatial_dims"],
+            in_channels=in_channels,
+            out_channels=out_channels,
+            channels=self._config["features"][1:],
+            strides=self._config["strides"][1:],
+            dropout=self._config["dropout"],
+            norm=self._config["norm"],
+            act=self._config["activation"],
+        )
+
+        def _create_block(
+            inc: int, outc: int, channels, strides, is_top: bool
+        ) -> nn.Module:
+            """
+            Builds the UNet structure from the bottom up by recursing down to the bottom block, then creating sequential
+            blocks containing the downsample path, a skip connection around the previous block, and the upsample path.
+
+            Args:
+                inc: number of input channels.
+                outc: number of output channels.
+                channels: sequence of channels. Top block first.
+                strides: convolution stride.
+                is_top: True if this is the top block.
+            """
+            c = channels[0]
+            s = strides[0]
+
+            subblock: nn.Module
+
+            if len(channels) > 2:
+                subblock = _create_block(c, c, channels[1:], strides[1:], False)  # continue recursion down
+                upc = c * 2
+            else:
+                # the next layer is the bottom so stop recursion, create the bottom layer as the sublock for this layer
+                subblock = self._get_bottom_layer(c, channels[1])
+                upc = c + channels[1]
+
+            down = self._get_down_layer(inc, c, s, is_top)  # create layer in downsampling path
+            up = self._get_up_layer(upc, outc, s, is_top)  # create layer in upsampling path
+
+            return self._get_connection_block(down, up, subblock)
+
+        self.model = _create_block(self._config["features"][0],self._config["features"][0],self._config["features"][1:], self._config["strides"][1:] , False)
+        down = self._get_down_layer(in_channels, 64, 1, True)
+        up = self._get_up_layer(self._config["features"][0]*2, out_channels, 1, True)
+        self.model = self._get_connection_block(down, up, self.model)
+        self.model = nn.Sequential(down)
+
+
+
+    def forward(self, x):
+        return self.model(x)
+    
 
             
 
--- src/test.py
+++ src/test.py
@@ -8,17 +8,23 @@ from torch.utils.data import DataLoader
 from trainer import Trainer
 from dataset.dataset import FemurImageDataset 
 from dataset.transforms import CustomCropRandomd
-from model.basic_UNet import BasicUNet, UpsampleUNet
+from model.basic_UNet import BasicUNet, UpsampleUNet, UpsampleUNet_new
 
 
 
 def main():
+
+
     torch.set_default_dtype(torch.float16)
+
     # Get Unet model from Monai
     CONFIG = yaml.safe_load(open("config/config.yaml", "r"))
     if "seed" in CONFIG:
         torch.manual_seed(CONFIG["seed"])
     print(CONFIG)
+    model = UpsampleUNet_new(1,1, CONFIG)
+    print(model(torch.randn(1,1,64,64,64)).shape)
+    exit()
     dataset = FemurImageDataset(config=CONFIG, split="train")
     val_conf = CONFIG.copy()
     val_conf["context_csv_path"] = "data/validation.csv"
