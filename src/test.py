cdf372e12865ab4549c3a8f1b6e91f606457ee21
Yannick Wattenberg <wattenberg@ywattenberg.com>
2023-12-26 13:25:08 +0100

    use dtype from config in setup

--- src/test.py
+++ src/test.py
@@ -10,26 +10,29 @@ from dataset.dataset import FemurImageDataset
 from dataset.transforms import CustomCropRandomd
 from model.basic_UNet import BasicUNet, UpsampleUNet, UpsampleUNet_new
 
+DATA_TYPES = {
+    "float32": torch.float32,
+    "float16": torch.float16,
+    "float64": torch.float64,
+    "float": torch.float32,
+}
 
 
 def main():
-
-
-    torch.set_default_dtype(torch.float16)
 
     # Get Unet model from Monai
     CONFIG = yaml.safe_load(open("config/config.yaml", "r"))
     if "seed" in CONFIG:
         torch.manual_seed(CONFIG["seed"])
     print(CONFIG)
-    model = UpsampleUNet_new(1,1, CONFIG)
-    print(model(torch.randn(1,1,64,64,64)).shape)
-    exit()
+    d_type = DATA_TYPES[CONFIG["dtype"]]
+    torch.set_default_dtype(d_type)
     dataset = FemurImageDataset(config=CONFIG, split="train")
     val_conf = CONFIG.copy()
     val_conf["context_csv_path"] = "data/validation.csv"
     val_dataset = FemurImageDataset(config=val_conf, split="val")
-    model = UpsampleUNet(1,1, CONFIG)
+    model = UpsampleUNet_new(1,1, CONFIG)
+
     # Load model
     # model.load_state_dict(torch.load("models/BasicUNet_epoch_5.pth"))
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-8)
