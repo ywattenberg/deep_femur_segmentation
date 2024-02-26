080798c8f345a1813028ae0d79993bc367c5af9e
Yannick Wattenberg <wattenberg@mailbox.org>
2024-02-26 14:08:14 +0100

    merge

--- scripts/simple_segmentation_main.py
+++ scripts/simple_segmentation_main.py
@@ -21,38 +21,36 @@ def main():
         torch.manual_seed(config["seed"])
     dataset = FemurSegmentationDataset(config, split="test")
 
-    for i in range(len(dataset)):
-        x, y, mask = dataset[i]
-        fig, ax = plt.subplots(2, 2, dpi=300, figsize=(10, 10))
-        ax[0, 0].imshow(x[0,0], cmap="gray")
-        ax[0,0].set_title("Input")
-        ax[0, 1].imshow(y[0,0], cmap="gray")
-        ax[0,1].set_title("Hr-pQCT")
-        ax[1, 0].imshow(mask[0, 0], cmap="gray")
-        ax[1,0].set_title("Cort")
-        ax[1, 1].imshow(mask[1, 0], cmap="gray")
-        ax[1,1].set_title("Trab")
-        plt.savefig(f"test/sample_{i}.png")
-        
+    # for i in range(len(dataset)):
+    #     x, y, mask = dataset[i]
+    #     fig, ax = plt.subplots(2, 2, dpi=300, figsize=(10, 10))
+    #     ax[0, 0].imshow(x[0,0], cmap="gray")
+    #     ax[0,0].set_title("Input")
+    #     ax[0, 1].imshow(y[0,0], cmap="gray")
+    #     ax[0,1].set_title("Hr-pQCT")
+    #     ax[1, 0].imshow(mask[0, 0], cmap="gray")
+    #     ax[1,0].set_title("Cort")
+    #     ax[1, 1].imshow(mask[1, 0], cmap="gray")
+    #     ax[1,1].set_title("Trab")
+    #     plt.savefig(f"test/sample_{i}.png")
 
-
-    # val_config = config.copy()
-    # val_config["context_csv_path"] = r"HRpQCT_aim\\numpy\\Cropped_regions_val.csv"
-    # val_dataset = FemurSegmentationDataset(val_config, split="val")
-    # model = monai_nets.UNet(
-    #     spatial_dims=config["model"]["spatial_dims"],
-    #     in_channels=1,
-    #     out_channels=1 if config["use_cortical_and_trabecular"] else 1,
-    #     channels=config["model"]["features"],
-    #     strides=config["model"]["strides"],
-    #     dropout=config["model"]["dropout"],
-    #     norm=config["model"]["norm"],
-    #     act=config["model"]["activation"],
-    # )
-    # model = model.to("cuda")
-    # optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-8)
-    # trainer = Trainer(model, dataset, val_dataset, DiceLoss(to_onehot_y=False, sigmoid=True), optimizer, config)
-    # trainer.train_test(epochs_between_test=50)
+    val_config = config.copy()
+    val_config["context_csv_path"] = r"HRpQCT_aim\\numpy\\Cropped_regions_val.csv"
+    val_dataset = FemurSegmentationDataset(val_config, split="val")
+    model = monai_nets.UNet(
+        spatial_dims=config["model"]["spatial_dims"],
+        in_channels=1,
+        out_channels=2 if config["use_cortical_and_trabecular"] else 1,
+        channels=config["model"]["features"],
+        strides=config["model"]["strides"],
+        dropout=config["model"]["dropout"],
+        norm=config["model"]["norm"],
+        act=config["model"]["activation"],
+    )
+    model = model.to("cuda")
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-8)
+    trainer = Trainer(model, dataset, val_dataset, DiceLoss(to_onehot_y=False, sigmoid=True), optimizer, config)
+    trainer.train_test(epochs_between_test=50)
 
 if __name__ == "__main__":
     main()
